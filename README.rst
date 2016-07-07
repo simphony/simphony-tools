4c8c6b1d77d1cfd4e759a1dd68f1edc5711db803
tuopuu <tuomas.a.puurtinen@jyu.fi>
2016-07-07 15:43:41 +0300

    Adding some standard files to simphony-tools reposity.

---
+++ README.rst
@@ -0,0 +1,10 @@
+simphony-tools
+==============
+
+Collection of general tools used in SimPhoNy project. These tools can be used,
+for instance, to analyse data in SimPhoNy framework.
+
+Repository
+----------
+
+Simphony-tools is hosted on github: https://github.com/simphony/simphony-tools
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = ''0.1.0.dev0'
+
+with open('README.rst', 'r') as readme:
+    README_TEXT = readme.read()
+
+with open("requirements.txt") as f:
+    INSTALL_REQUIRES = f.readlines()
+
+# main setup configuration class
+setup(
+    name='SimPhoNy Tools Collection',
+    version=VERSION,
+    author='SimPhoNy, EU FP7 Project (Nr. 604005) www.simphony-project.eu',
+    description='Collection of tools for SimPhoNy',
+    long_description=README_TEXT,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+)
